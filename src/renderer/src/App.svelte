bd468e488431b22307fed920841decaf55c1d9d1
nfoert <nofoert@gmail.com>
2025-07-23 12:10:13 -0400

    feat: Add basic expandable button guide

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import Clock from './components/Clock.svelte'
+  import Clock from './components/Clock.svelte' 
+  import Buttons from './components/Buttons.svelte';
 
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
 <div class="flex justify-center items-center bg-slate-800 w-screen h-screen">
+  <Buttons></Buttons>
   <Clock></Clock>
 </div>
---
+++ src/renderer/src/components/Buttons.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let open = false;
+
+    $: style = open
+    ? "flex flex-col absolute top-0 left-0 h-screen justify-between py-8 pl-2 bg-gradient-to-r from-black/50 to-transparent pr-10 backdrop-blur-xs transition-all"
+    : "flex flex-col absolute top-0 left-0 h-screen justify-between py-8 pl-2 opacity-50 transition-all";
+</script>
+
+<div class={style}>
+    <div class="flex flex-row gap-1 text-white items-center">
+        <i class="ph-bold ph-circle"></i>
+        <p class={open ? '' : 'hidden'}>Button 1</p>
+    </div>
+
+    <div class="flex flex-row gap-1 text-white items-center">
+        <i class="ph-bold ph-circle"></i>
+        <p class={open ? '' : 'hidden'}>Button 2</p>
+    </div>
+
+    <div class="flex flex-row gap-1 text-white items-center">
+        <i class="ph-bold ph-circle"></i>
+        <p class={open ? '' : 'hidden'}>Button 3</p>
+    </div>
+
+    <div class="flex flex-row gap-1 text-white items-center">
+        <i class="ph-bold ph-circle"></i>
+        <p class={open ? '' : 'hidden'}>Button 4</p>
+    </div>
+</div>
