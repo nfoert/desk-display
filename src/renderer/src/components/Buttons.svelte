6f6b5bf6e6a90245bc167b7989446fafedc82e34
nfoert <nofoert@gmail.com>
2025-08-10 14:56:35 -0400

    feat: Be able to set button modes to change icons and text of the prompts

--- src/renderer/src/components/Buttons.svelte
+++ src/renderer/src/components/Buttons.svelte
@@ -1,8 +1,55 @@
 <script>
+  import { onMount } from "svelte"
+
     let open = false;
+
+    let modes = {
+        "main": {
+            "button-1": {
+                "icon": "",
+                "text": "",
+                "action": ""
+            },
+            "button-2": {
+                "icon": "",
+                "text": "",
+                "action": ""
+            },
+            "button-3": {
+                "icon": "",
+                "text": "",
+                "action": ""
+            },
+            "button-4": {
+                "icon": "ph-bold ph-squares-four",
+                "text": "Menu",
+                "action": "menu"
+            }
+        }
+    }
+
+    function setMode(mode) {
+        if (mode in modes) {
+            document.querySelector("#button-1 p").innerHTML = modes[mode]["button-1"]["text"];
+            document.querySelector("#button-1 > i").classList = modes[mode]["button-1"]["icon"];
+
+            document.querySelector("#button-2 p").innerHTML = modes[mode]["button-2"]["text"];
+            document.querySelector("#button-2 > i").classList = modes[mode]["button-2"]["icon"];
+
+            document.querySelector("#button-3 p").innerHTML = modes[mode]["button-3"]["text"];
+            document.querySelector("#button-3 > i").classList = modes[mode]["button-3"]["icon"];
+
+            document.querySelector("#button-4 p").innerHTML = modes[mode]["button-4"]["text"];
+            document.querySelector("#button-4 > i").classList = modes[mode]["button-4"]["icon"];
+        }
+    }
+
+    onMount(() => {
+        setMode("main");
+    });
 </script>
 
-<div class="flex flex-col absolute top-0 left-0 h-screen z-10 py-8 pl-2
+<div class="flex flex-col absolute top-0 left-0 h-screen z-10 py-8 pl-4
             bg-gradient-to-r from-10% to-transparent to-90% backdrop-blur-sm
             transition-all duration-500 ease-in-out justify-between px-4"
      class:w-screen={open}
@@ -11,14 +58,13 @@
      class:from-transparent={!open}>
     
     {#each [1, 2, 3, 4] as num}
-        <div class="flex flex-row items-center text-white transition-all duration-500 ease-in-out">
+        <div class="flex flex-row items-center text-white transition-all duration-500 ease-in-out" id={`button-${num}`}>
             <i class="ph-bold ph-circle transition-opacity duration-300 ease-in-out" 
                class:opacity-100={open} 
-               class:opacity-50={!open}></i>
+               class:opacity-30={!open}></i>
 
             <div class="ml-2 overflow-hidden transition-all duration-500 ease-in-out"
-                 style="max-width: {open ? '120px' : '0px'}"
-                 id="button-{num}">
+                 style="max-width: {open ? '120px' : '0px'}">
                 <p class="whitespace-nowrap transition-opacity duration-300 ease-in-out"
                    class:opacity-100={open}
                    class:opacity-0={!open}>
