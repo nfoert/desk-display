61b2ef8f01f1e87e3f6e4ff33168f46a6216cb4d
nfoert <nofoert@gmail.com>
2025-08-10 12:37:14 -0400

    feat: Better animate button prompts

--- src/renderer/src/components/Buttons.svelte
+++ src/renderer/src/components/Buttons.svelte
@@ -1,29 +1,30 @@
-<script lang="ts">
+<script>
     let open = false;
-
-    $: style = open
-    ? "flex flex-col absolute top-0 left-0 h-screen justify-between py-8 pl-2 bg-gradient-to-r from-black/50 to-transparent pr-10 backdrop-blur-xs transition-all"
-    : "flex flex-col absolute top-0 left-0 h-screen justify-between py-8 pl-2 opacity-50 transition-all";
 </script>
 
-<div class={style}>
-    <div class="flex flex-row gap-1 text-white items-center">
-        <i class="ph-bold ph-circle"></i>
-        <p class={open ? '' : 'hidden'}>Button 1</p>
-    </div>
+<div class="flex flex-col absolute top-0 left-0 h-screen z-10 py-8 pl-2
+            bg-gradient-to-r from-10% to-transparent to-90% backdrop-blur-sm
+            transition-all duration-500 ease-in-out justify-between px-4"
+     class:w-screen={open}
+     class:w-20={!open}
+     class:from-slate-900={open}
+     class:from-transparent={!open}>
+    
+    {#each [1, 2, 3, 4] as num}
+        <div class="flex flex-row items-center text-white transition-all duration-500 ease-in-out">
+            <i class="ph-bold ph-circle transition-opacity duration-300 ease-in-out" 
+               class:opacity-100={open} 
+               class:opacity-50={!open}></i>
 
-    <div class="flex flex-row gap-1 text-white items-center">
-        <i class="ph-bold ph-circle"></i>
-        <p class={open ? '' : 'hidden'}>Button 2</p>
-    </div>
-
-    <div class="flex flex-row gap-1 text-white items-center">
-        <i class="ph-bold ph-circle"></i>
-        <p class={open ? '' : 'hidden'}>Button 3</p>
-    </div>
-
-    <div class="flex flex-row gap-1 text-white items-center">
-        <i class="ph-bold ph-circle"></i>
-        <p class={open ? '' : 'hidden'}>Button 4</p>
-    </div>
+            <div class="ml-2 overflow-hidden transition-all duration-500 ease-in-out"
+                 style="max-width: {open ? '120px' : '0px'}"
+                 id="button-{num}">
+                <p class="whitespace-nowrap transition-opacity duration-300 ease-in-out"
+                   class:opacity-100={open}
+                   class:opacity-0={!open}>
+                    Button {num}
+                </p>
+            </div>
+        </div>
+    {/each}
 </div>
